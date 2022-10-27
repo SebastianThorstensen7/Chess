9dba10c146cd5a76ebc8d6c8fb0b77fb41f576ba
Sebastian Thorstensen <93603900+SebastianThorstensen7@users.noreply.github.com>
2022-10-27 17:28:59 +0000

    updated main to allow for 100 loops

--- Main.java
+++ Main.java
@@ -17,33 +17,37 @@ class Main {
     ArrayList<Piece> Pieces = new ArrayList<Piece>();
 
     for (int i = 0; i < 8; i++) {
-      Pieces.add(new Pawn(true, "P" + i, 1, i));
+      Pieces.add(new Pawn(true, "P" + (i + 1), 1, i));
     }
     for (int k = 0; k < 2; k++) {
-      Pieces.add(new Rook(true, "R" + k, 0, (k * 7)));
-      Pieces.add(new Knight(true, "KN" + k, 0, (k * 5) + 1));
-      Pieces.add(new Bishop(true, "B" + k, 0, (k * 3) + 2));
+      Pieces.add(new Rook(true, "R" + (k + 1), 0, (k * 7)));
+      Pieces.add(new Knight(true, "KN" + (k + 1), 0, (k * 5) + 1));
+      Pieces.add(new Bishop(true, "B" + (k + 1), 0, (k * 3) + 2));
     }
     Pieces.add(new King(true, "K", 0, 4));
     Pieces.add(new Queen(true, "Q", 0, 3));
 
     for (int j = 0; j < 8; j++) {
-      Pieces.add(new Pawn(false, "P" + j, 6, j));
+      Pieces.add(new Pawn(false, "P" + (j + 1), 6, j));
     }
     for (int k = 0; k < 2; k++) {
-      Pieces.add(new Rook(false, "R" + k, 7, (k * 7)));
-      Pieces.add(new Knight(false, "KN" + k, 7, (k * 5) + 1));
-      Pieces.add(new Bishop(false, "B" + k, 7, (k * 3) + 2));
+      Pieces.add(new Rook(false, "R" + (k + 1), 7, (k * 7)));
+      Pieces.add(new Knight(false, "KN" + (k + 1), 7, (k * 5) + 1));
+      Pieces.add(new Bishop(false, "B" + (k + 1), 7, (k * 3) + 2));
     }
     Pieces.add(new King(false, "K", 7, 4));
     Pieces.add(new Queen(false, "Q", 7, 3));
 
     Board Board = new Board(Pieces);
-    Board.selectPiece();
-    Board.updateState();
-    Board.switchTeam();
-    Board.selectPiece();
-    Board.updateState();
+    
+    for(int i = 0; i < 100; i++) {
+      Board.selectPiece();
+      Board.updateState();
+      Board.switchTeam();
+    }
+    
+
+
     
 
     System.out.println();
