03e37c02977c24fc315e47b194fff2df5dbda018
Khushi  Singh <70658902+Khushi-NSD@users.noreply.github.com>
2023-01-10 18:58:50 +0000

    the rook works i mean kinda...need to make the code more efficient tho

--- src/main/java/edu/bothell/chess/Rook.java
+++ src/main/java/edu/bothell/chess/Rook.java
@@ -14,12 +14,10 @@ import java.util.Scanner;
 
 public class Rook extends Piece {
 
-
-
-  public Rook(boolean team, Square square, B board){
+  public Rook(boolean team, Square square, B board) {
     super(team, square, board);
   }
-  
+
   public void move(Scanner scn) {
     super.move(scn);
     System.out.println("Rook move");
@@ -31,16 +29,62 @@ public class Rook extends Piece {
    * Starting coordinates are now got with super.getRow() and super.getColumn()
    * Destination coordinates are now got with x.getRow() and x.getColumn()
    * Don't worry about the check Inbetween method I'll be working on it
+
+
    */
   public boolean validateMove(Square x) {
       boolean validMove       = false;
-      //code goes here
-      boolean noPeacesBetween = checkInbetween(x);
-      if(validMove && noPeacesBetween) {
-        return true;
+      if (x.isEmpty()) {
+      if (super.getRow()  != x.getRow() && super.getColumn() != x.getColumn()) {
+        return false;
+      } else if (super.getRow()  != x.getRow() && super.getColumn() == x.getColumn()) {
+        if (super.getRow()  < x.getRow()) {
+          for (int i = super.getRow(); i < x.getColumn(); i++) {
+            if (x.isEmpty()) {
+              return false;
+            } else if (x.getRow() - 1 == i) {
+              validMove = true;
+              return validMove;
+            }
+          }
+        } else if (super.getRow()  > x.getRow()) {
+          for (int i = super.getRow() ; i > x.getRow(); i--) {
+            if (x.isEmpty()) {
+              return false;
+            } else if (x.getRow() - 1 == i) {
+              validMove = true;
+              return validMove;
+            }
+          }
+        }
+        validMove = true;
+        return validMove;
+      } else {
+        validMove = true;
+        return validMove;
+      }}
+
+
+    else if (super.getRow()  == x.getRow() && super.getColumn() != x.getColumn()) {
+      if (super.getColumn() > x.getColumn()) {
+        for (int i = super.getColumn(); i > x.getColumn(); i--) {
+          if ((x.isEmpty())) {
+            return false;
+          }
+        }
+      } else if (super.getColumn() < x.getColumn()) {
+        for (int i = super.getColumn(); i < x.getColumn(); i++) {
+          if ((x.isEmpty())) {
+            return false;
+          }
+        }
       }
-      return false;
-    }
+      return true;
+    } else {
+      validMove = true;
+      return validMove;
+    }}
+  
 
   public String toString() {
     if (super.getTeam()) {
