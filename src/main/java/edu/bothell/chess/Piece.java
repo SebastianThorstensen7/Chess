36b225070aca23203b5e79d09c8046494ec0a9bb
Sebastian Thorstensen <93603900+SebastianThorstensen7@users.noreply.github.com>
2023-01-12 18:18:22 +0000

    bishop test is back!

--- src/main/java/edu/bothell/chess/Piece.java
+++ src/main/java/edu/bothell/chess/Piece.java
@@ -1,4 +1,5 @@
 package edu.bothell.chess;
+
 import java.util.Scanner;
 
 public class Piece {
@@ -11,10 +12,11 @@ public class Piece {
   /**
    * GUS: New constructore for a Piece -- adding a board and removing the ID
    * rows and columns seem backwards to me?
-   * @param  team    boolean  
-   * @param  row     int
-   * @param  column  int
-   * @param  board   Board type with Square[][] and ArrayList<Piece>
+   * 
+   * @param team   boolean
+   * @param row    int
+   * @param column int
+   * @param board  Board type with Square[][] and ArrayList<Piece>
    */
   public Piece(boolean team, Square square, B state) {
     this.team = team;
@@ -22,21 +24,21 @@ public class Piece {
     this.state = state;
   }
 
-  
   ///////////////////////////////////////////////
   // METHODS of CHESS PIECE(s)
   ///////////////////////////////////////////////
   /**
-   * GUS: Starts the process of checking a move. 
+   * GUS: Starts the process of checking a move.
    * Can Override for specific pieces
-   * @param  x    X marks the spot of the Square to move to
+   * 
+   * @param x X marks the spot of the Square to move to
    * @retrun boolean saying if the move is OK
    */
 
-  public void setSquare(Square square){
+  public void setSquare(Square square) {
     this.square = square;
   }
-  
+
   public void move(Scanner scn) {
 
   }
@@ -44,29 +46,51 @@ public class Piece {
   // validate move method
   // Super method is king movement
   public boolean validateMove(Square x) {
-    if( x.isTeam(team) ) return false;
+    if (x.isTeam(team))
+      return false;
     if (this.getColumn() != x.getColumn() && this.getRow() != x.getRow()) {
-        if ((this.getColumn() + 1 == x.getColumn()) || (this.getColumn() - 1 == x.getColumn())) {
-          if ((this.getRow() + 1 == x.getRow()) || (this.getRow() - 1 == x.getRow() )) {
-            return true;
-          }
-        } 
-      } else if((this.getColumn() != x.getColumn()) && (this.getRow() == x.getRow()) ) {
-        if(this.getColumn() + 1 == x.getColumn() || this.getColumn() - 1 == x.getColumn()) {
+      if ((this.getColumn() + 1 == x.getColumn()) || (this.getColumn() - 1 == x.getColumn())) {
+        if ((this.getRow() + 1 == x.getRow()) || (this.getRow() - 1 == x.getRow())) {
           return true;
-        } 
-      } else if((this.getColumn() == x.getColumn()) && (this.getRow() != x.getRow())) {
-        if ((this.getRow() + 1 == x.getRow()) || (this.getRow() - 1 == x.getRow() )) {
-            return true;
-        } 
-      } 
+        }
+      }
+    } else if ((this.getColumn() != x.getColumn()) && (this.getRow() == x.getRow())) {
+      if (this.getColumn() + 1 == x.getColumn() || this.getColumn() - 1 == x.getColumn()) {
+        return true;
+      }
+    } else if ((this.getColumn() == x.getColumn()) && (this.getRow() != x.getRow())) {
+      if ((this.getRow() + 1 == x.getRow()) || (this.getRow() - 1 == x.getRow())) {
+        return true;
+      }
+    }
     return false;
   }
 
-  //Method to check if there a pieces between a location and destination
+  // Method to check if there a pieces between a location and destination
   public boolean checkInbetween(Square end) {
-    
+    int rowDirect = getDirection(this.getRow() - end.getRow());
+    int columnDirect = getDirection(this.getColumn() - end.getColumn());
+    for(int x = this.getRow(); x < end.getRow(); x = x + rowDirect) {
+      for(int y = this.getColumn(); y < end.getColumn(); y = x + columnDirect) {
+        Square square = state.getSquare(x,y);
+        if(!square.isEmpty()) {
+          System.out.println("Piece between");
+          return false;
+        }
+      }
+    }
+    System.out.println("No pieces between");
     return true;
+  }
+
+  public int getDirection(int direction) {
+    if (direction <= -1) {
+      return -1;
+    } else if (direction >= 1) {
+      return 1;
+    } else {
+      return 0;
+    }
   }
 
   public void setRow(int row) {
