fbf267e15ba20e06e3d8a6fd4773b22b6e3553eb
Manas Kumar <70679511+Manas1114880@users.noreply.github.com>
2023-02-03 19:07:01 +0000

    I think the tests for Queen work now

--- src/test/java/edu/bothell/chess/QueenTest.java
+++ src/test/java/edu/bothell/chess/QueenTest.java
@@ -1,7 +1,47 @@
+
 package edu.bothell.chess;
 
-import org.junit.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import java.util.*;
+
+import org.junit.Test;
 
 public class QueenTest {
-  // Needs 2 tests
+  B board;
+
+  @Before
+  public void setUp() {
+    ArrayList<Piece> pieces = new ArrayList<Piece>();
+    pieces.add(new Queen(true, new Square(4, 7), board));
+    pieces.add(new Rook(false, new Square(5, 6), board));
+    pieces.add(new Rook(true, new Square(1, 7), board));
+    pieces.add(new Rook(false, new Square(4, 2), board));
+    pieces.add(new Rook(true, new Square(4, 3), board));
+    pieces.add(new Rook(false, new Square(4, 4), board));
+    pieces.add(new Rook(true, new Square(4, 5), board));
+    pieces.add(new Rook(true, new Square(3, 3), board));
+    //pieces.add(new Rook(false, new Square(1, 2), board));
+    //pieces.add(new Rook(true, new Square(1, 2), board));
+    
+    board = new B(pieces);
+    for(Piece p: pieces) {
+      p.setBoard(board);
+    }
+  }
+
+    @Test
+  public void queenShouldAnswerWithTrue() {
+    assertTrue(board.checkMove(4,7,5,6));
+    assertTrue(board.checkMove(4,7,3,6));
+  }
+
+  @Test
+  public void queenShouldAnswerWithFalse() {
+    assertFalse(board.checkMove(4,7,4,3));
+    assertFalse(board.checkMove(4,7,3,3));
+    assertFalse(board.checkMove(4,7,4,5));
+
+  }
 }
